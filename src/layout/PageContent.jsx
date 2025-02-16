a4177421d8d5235c03fb0164375f5ce7788e0433
hande <hande.dann@gmail.com>
2025-02-16 14:36:22 +0300

    ContactPage routing

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ShopPage from "../pages/ShopPage";
 import ProductDetailPage from "../pages/ProductDetailPage";
+import ContactPage from "../pages/ContactPage";
 
 const PageContent = () => {
   return (
@@ -12,6 +13,7 @@ const PageContent = () => {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
           <Route path="/product" element={<ProductDetailPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </>
     </div>
