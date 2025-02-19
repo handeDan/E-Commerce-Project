bf0ca3a06b8b6c2a550f20e4fe47f5f146256632
hande <hande.dann@gmail.com>
2025-02-19 12:46:59 +0300

    AboutPage / Video component

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Main from "../components/AboutPage/Main";
 import Description from "../components/AboutPage/Description";
+import Video from "../components/AboutPage/Video";
 
 function AboutPage() {
   return (
     <div>
       <Main />
       <Description />
+      <Video />
     </div>
   );
 }
